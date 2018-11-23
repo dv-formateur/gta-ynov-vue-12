c3c07b507c8d48d98b895ca55372ccddfd2c5cb7
masaintmartin <matthieu.saint-martin@ynov.com>
2018-11-23 08:52:07 +0100

    Try push heroku

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-// server.js
-var express = require('express');
-var serveStatic = require('serve-static');
-const app = express();
-app.use(serveStatic(__dirname + '/dist'));
-var port = process.env.PORT || 5000;
-app.listen(port);
